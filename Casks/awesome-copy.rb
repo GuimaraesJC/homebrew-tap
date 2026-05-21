36f20e4bc0a47dee41b2396fe2fffd9d85041d98
kindparkllc <kindparkllc@gmail.com>
2026-05-21 14:48:30 -0400

    Update Awesome Copy cask to 6.1.2 (build 367)

--- Casks/awesome-copy.rb
+++ Casks/awesome-copy.rb
@@ -1,8 +1,8 @@
 cask "awesome-copy" do
-  version "6.1,357"
-  sha256 "3351a723e1c59f37aff53fc05a301b566f1a22907c2fe96a12822aaeb2027504"
+  version "6.1.2,367"
+  sha256 "88bb89f2c7a259f287135181768b73e3b6a2fbf73f96c3e0f6f813157ac87e8a"
 
-  url "https://github.com/kindparkllc/Awesome-Copy/releases/download/v#{version.before_comma}/AwesomeCopy-#{version.before_comma}-#{version.after_comma}.dmg"
+  url "https://github.com/kindparkllc/Awesome-Copy/releases/download/v\#{version.before_comma}/AwesomeCopy-\#{version.before_comma}-\#{version.after_comma}.dmg"
   name "Awesome Copy"
   desc "Clipboard manager for Mac"
   homepage "https://awesomecopy.app"
