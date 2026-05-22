5bafd1b46cfaa668d9f5a52f0dea7a76eb4d3951
kindparkllc <kindparkllc@gmail.com>
2026-05-22 15:55:41 -0400

    Add awesome-copy-beta cask for beta channel (6.2b1 build 369)

---
+++ Casks/awesome-copy-beta.rb
@@ -0,0 +1,19 @@
+cask "awesome-copy-beta" do
+  version "6.2b1,369"
+  sha256 "22a31a8b08c99abc146b5c16f78f4a9171c75335e56e51ee00231d0d478f0124"
+
+  url "https://github.com/kindparkllc/Awesome-Copy/releases/download/Beta/AwesomeCopy-\#\{version.before_comma\}-\#\{version.after_comma\}.dmg"
+  name "Awesome Copy"
+  desc "Clipboard manager for Mac (beta)"
+  homepage "https://awesomecopy.app"
+
+  conflicts_with cask: "awesome-copy"
+
+  app "Awesome Copy.app"
+
+  zap trash: [
+    "~/Library/Containers/com.kindpark.AwesomeCopy",
+    "~/Library/Application Support/AwesomeCopy",
+    "~/Library/Preferences/com.kindpark.AwesomeCopy.plist",
+  ]
+end
