d2afc2362d2d177bab17cd88c7df1894dba93d17
kindparkllc <kindparkllc@gmail.com>
2026-05-22 20:11:17 -0400

    Fix url interpolation: use #{} instead of \#{}

--- Casks/awesome-copy.rb
+++ Casks/awesome-copy.rb
@@ -2,7 +2,7 @@ cask "awesome-copy" do
   version "6.1.2,367"
   sha256 "88bb89f2c7a259f287135181768b73e3b6a2fbf73f96c3e0f6f813157ac87e8a"
 
-  url "https://github.com/kindparkllc/Awesome-Copy/releases/download/v\#{version.before_comma}/AwesomeCopy-\#{version.before_comma}-\#{version.after_comma}.dmg"
+  url "https://github.com/kindparkllc/Awesome-Copy/releases/download/v#{version.before_comma}/AwesomeCopy-#{version.before_comma}-#{version.after_comma}.dmg"
   name "Awesome Copy"
   desc "Clipboard manager for Mac"
   homepage "https://awesomecopy.app"
