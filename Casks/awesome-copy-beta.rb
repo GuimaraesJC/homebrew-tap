da3536cba07e902ce1620aabb16d54cc720727fd
kindparkllc <kindparkllc@gmail.com>
2026-06-17 00:21:39 -0400

    Update Awesome Copy beta cask to 7.0b3 (build 377)

--- Casks/awesome-copy-beta.rb
+++ Casks/awesome-copy-beta.rb
@@ -1,6 +1,6 @@
 cask "awesome-copy-beta" do
-  version "7.0b2,377"
-  sha256 "b077f2798e89dce7de840cac67a64648dfc07eec6d245513564467da1e9463c2"
+  version "7.0b3,377"
+  sha256 "0c4d54424c93fd0e235d01a0f85f007bc4439d6dcb367cf42342146b6bd3505a"
 
   url "https://github.com/kindparkllc/Awesome-Copy/releases/download/Beta/AwesomeCopy-\#{version.before_comma}-\#{version.after_comma}.dmg"
   name "Awesome Copy"
